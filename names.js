234faa9f9fd9bff67af5cef1cb296a13224ec9c6
nvas2305 <nguyenvananh23052008@gmail.com>
2026-02-26 00:43:04 +0700

    Add files via upload

---
+++ names.js
@@ -0,0 +1,23 @@
+// names.js
+window.NAME_TO_IMAGE = {
+  "Phạm Thái Bình": "binh.png",
+  "Thái Bình": "binh.png",
+  "bình": "binh.png",
+  "Bình": "binh.png",
+  "thái bình": "binh.png",
+  "phạm thái bình": "binh.png",
+
+  "Nguyễn Quốc Sang": "sang.png",
+  "Quốc Sang": "sang.png",
+  "sang": "sang.png",
+  "Sang": "sang.png",
+  "quốc sang": "sang.png",
+  "nguyễn quốc sang": "sang.png",
+
+  "Phạm Trọng Thành": "thanh.png",
+  "Trọng Thành": "thanh.png",
+  "thành": "thanh.png",
+  "Thành": "thanh.png",
+  "trọng thành": "thanh.png",
+  "phạm trọng thành": "thanh.png",
+};
